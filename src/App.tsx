766e6ee38a78c716be2eb14547cd67232e2d0c1d
Vishu Kumar <1992vishukumar@gmail.com>
2024-05-02 09:34:33 +0530

    Videos section implemented

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import "./App.css";
 import CategoryPills from "./components/CategoryPills";
 import { PageHeader } from "./components/layout/PageHeader";
-import { categories } from "./data/home";
+import { categories, videos } from "./data/home";
+import VideoGridItem from "./components/VideoGridItem";
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState(categories[0]);
@@ -19,6 +20,11 @@ function App() {
               selectedCategory={selectedCategory}
               onSelect={setSelectedCategory}
             />
+          </div>
+          <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))]">
+            {videos.map((video) => (
+              <VideoGridItem key={video.id} {...video} />
+            ))}
           </div>
         </div>
       </div>
